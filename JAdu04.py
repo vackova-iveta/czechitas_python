d736805ece94151e76de16794f29b4802e7ef569
vackova-iveta <82833857+vackova-iveta@users.noreply.github.com>
2021-05-06 13:10:20 +0200

    Update JAdu04.py

--- JAdu04.py
+++ JAdu04.py
@@ -1,20 +1,10 @@
 # Napiš funkce monthOfBirth, která bude mít jeden parametr - rodné číslo. Funkce ze zadaného rodného čísla určí měsíc narození, které vrátí jako výstup. Nezapomeň, že pro ženy je k měsíci připočtena hodnota 50. Příklad: Pro hodnotu 9207054439 vrátí 7. Pro hodnotu 9555125899 vrátí 5.
 
-def monthOfBirth(rc):
-  print(f"Mesic narozeni je: {}")
-
-  if rc[2:3] > 50:
-    mesic = rc[]
-  else:
-    mark = 1
-  return monthOfBirth_int
-
-##################################################
 RC = input("Zadej celé rodne cislo bez lomítka: ")
 # část kodu pro rok
 yearOfBirth = RC[0:2]
 yearOfBirth_int = int(yearOfBirth)
-if rok_int < 22:
+if yearOfBirth_int < 22:
     print("rok:", 2000 + yearOfBirth_int)  # Milenial
 else:
     print("rok:", 1900 + yearOfBirth_int)  # Nemilenial
