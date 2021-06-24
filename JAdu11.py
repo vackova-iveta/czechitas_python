7c82350cce4ec2bcc1585c30edd71bb1db0b2f6d
vackova-iveta <82833857+vackova-iveta@users.noreply.github.com>
2021-06-24 19:26:46 +0200

    Update JAdu11.py

--- JAdu11.py
+++ JAdu11.py
@@ -7,7 +7,6 @@ emailSRadami = """
 Ahoj,
 posílám ti pár tipů, kam se podívat. https://realpython.com nabízí spoustu článků i kurzů. http://docs.python.org nabízí tutoriál i rozsáhlou dokumentaci. http://www.learnpython.org nabízí hezky strukturovaný kurz pro začátečníky, rozebírá ale i nějaká pokročilejší témata. https://www.pluralsight.com je placený web, který ale kvalitou kurzů víceméně nemá konkurenci. Určitě ale sleduj i web https://www.czechitas.cz.
 """
-weby = re.compile(r"([\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/\?\=\&\#\.]?[\w-]+)*\/?")
 
-for e in weby.findall(emailSRadami):
-    print(e)
+urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', emailSRadami)
+print(urls)
